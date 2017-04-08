28a4dd14fc9ea7e6254eb33e7b7c137b9d87266a
StickNitro <stevensn99@gmail.com>
2017-04-08 15:47:28 +0100

    minified code blocks and added log4js logging, added certificate properties to configuration

--- controllers/index.js
+++ controllers/index.js
@@ -1,32 +1,16 @@
+const log4js = require("log4js");
 const express = require("express");
+
+const log = log4js.getLogger("controllers");
 
 const router = express.Router();
 
-const fs = require("fs");
-const path = require("path");
-function loadControllers(rootPath, parent) {
-    parent = parent || "";
-    const fi = fs.lstatSync(rootPath);
-    if (fi.isDirectory()) {
-        const files = fs.readdirSync(rootPath);
-        const apiPath = rootPath.substring(rootPath.lastIndexOf("\\")).replace("\\", "/");
-        parent += apiPath;
-        for (var x = 0; x < files.length; x++) {
-            const file = path.join(rootPath, files[x]);
-            loadControllers(file, parent);
-        }
-    } else {
-        loadController(rootPath, parent);
-    }
-}
+/* ##### DO NOT CHANGE ANYTHING BELOW THIS LINE ##### */
+/*This is the middleware loader, it will dynamically load middleware instances from the nested folder structure below this folder, the only caveat is that the middleware file must contain the word Middleware in its name */const fs = require("fs");const path = require("path");function loadControllers(rootPath, parent) {parent = parent || "";const fi = fs.lstatSync(rootPath);if (fi.isDirectory()) {const files = fs.readdirSync(rootPath);const apiPath = rootPath.substring(rootPath.lastIndexOf("\\")).replace("\\", "/");parent += apiPath;for (var x = 0; x < files.length; x++) {const file = path.join(rootPath, files[x]);loadControllers(file, parent);}} else {loadController(rootPath, parent);}}function loadController(fileName, apiPath) {const basename = path.basename(fileName);if (basename.indexOf("Controller") !== -1) {log.info(`registering middleware ${filename}`);router.use(parent, require("." + parent + "/" + path.basename(filename, ".js")));}}loadControllers(path.join(__dirname, "api"));
+/* ##### DO NOT CHANGE ANYTHING ABOVE THIS LINE ##### */
 
-function loadController(fileName, apiPath) {
-    const basename = path.basename(fileName);
-    if (basename.indexOf("Controller") !== -1) {
-        console.info(`registering middleware ${filename}`);
-        router.use(parent, require("." + parent + "/" + path.basename(filename, ".js")));
-    }
-}
-loadControllers(path.join(__dirname, "api"));
+router.get("*", (req, res) => {
+    res.status(404).send();
+});
 
 module.exports = router;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,36 +1,24 @@
+const log4js = require("log4js");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const log = log4js.getLogger("middlewares");
 
 const router = express.Router();
 
 // set CORS for all routes
+log.info("register cors");
 router.all("*", cors());
 
-const fs = require("fs");
-const path = require("path");
-function loadMiddlewares(rootPath, parent) {
-    parent = parent || "";
-    const fi = fs.lstatSync(rootPath);
-    if (fi.isDirectory()) {
-        const files = fs.readdirSync(rootPath);
-        const apiPath = rootPath.substring(rootPath.lastIndexOf("\\")).replace("\\", "/");
-        parent += apiPath;
-        for (var x = 0; x < files.length; x++) {
-            const file = path.join(rootPath, files[x]);
-            loadMiddlewares(file, parent);
-        }
-    } else {
-        loadMiddleware(rootPath, parent);
-    }
-}
+log.info("register body parser");
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+router.use(bodyParser.json());
 
-function loadMiddleware(fileName, apiPath) {
-    const basename = path.basename(fileName);
-    if (basename.indexOf("Middleware") !== -1) {
-        console.info(`registering middleware ${filename}`);
-        router.use(require("." + parent + "/" + path.basename(filename, ".js")));
-    }
-}
-loadMiddlewares(path.join(__dirname, "api"));
+/* ##### DO NOT CHANGE ANYTHING BELOW THIS LINE ##### */
+/* This is the middleware loader, it will dynamically load middleware instances from the nested folder structure below this folder, the only caveat is that the middleware file must contain the word Middleware in its name */const fs = require("fs");const path = require("path");function loadMiddlewares(rootPath, parent) {parent = parent || "";const fi = fs.lstatSync(rootPath);if (fi.isDirectory()) {const files = fs.readdirSync(rootPath);const apiPath = rootPath.substring(rootPath.lastIndexOf("\\")).replace("\\", "/");parent += apiPath;for (var x = 0; x < files.length; x++) {const file = path.join(rootPath, files[x]);loadMiddlewares(file, parent);}} else {loadMiddleware(rootPath, parent);}}function loadMiddleware(fileName, apiPath) {const basename = path.basename(fileName);if (basename.indexOf("Middleware") !== -1) {log.info(`registering middleware ${filename}`);router.use(require("." + parent + "/" + path.basename(filename, ".js")));}}loadMiddlewares(path.join(__dirname, "api"));
+/* ##### DO NOT CHANGE ANYTHING ABOVE THIS LINE ##### */
 
 module.exports = router;
